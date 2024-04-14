acc1dc93c3e5e635495d7ee891b220afa1c73966
hassansohaill <101779750+hassansohaill@users.noreply.github.com>
2024-04-13 21:01:51 -0600

    Profile page starter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
+import PerformanceReportPage from "./Pages/PerformanceReportPage";
+import ProfilePage from "./Pages/ProfilePage";
 
 export default function App() {
 	return (
@@ -13,6 +15,8 @@ export default function App() {
 					<Route path="/login" element={<LoginPage />} />
 					<Route path="/register" element={<RegisterPage />} />
 					<Route path="/homepage/:userId" element={<HomePage />} />
+					<Route path="/performancereport" element={<PerformanceReportPage />} />
+					<Route path="/profilepage" element={<ProfilePage />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
