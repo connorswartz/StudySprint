e71f16fc2c4ea3cfe11446a683fb22a4bef8f1ce
Connor Swartz <connoreswartz@gmail.com>
2024-04-16 23:19:37 -0600

    Delete .history directory

--- .history/frontend/src/components/ListboxWrapper_20240416125819.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const ListboxWrapper = ({ children }) => (
-  <div className="w-[260px] border-large px-1 py-2 rounded-small border-blue-900">
-    {children}
-  </div>
-);
